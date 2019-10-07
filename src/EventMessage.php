3920450484c5fbba43745561a9ea39672678deef
Burov Alexey <A.Burov@petrosoftinc.com>
2019-10-07 12:57:21 +0300

    new properties

--- src/EventMessage.php
+++ src/EventMessage.php
@@ -5,13 +5,19 @@ namespace Nachtiga11\EventMessage\Src;
 /**
  * Class EventMessage
  */
-class EventMessage
+final class EventMessage
 {
     public const VERSION = 1;
 
     private $token;
 
     private $status;
+
+    private $serviceId;
+
+    private $nodeId;
+
+    private $createdAt;
 
     private $message;
 
@@ -42,6 +48,21 @@ class EventMessage
         return $this->body;
     }
 
+    public function getServiceId()
+    {
+        return $this->serviceId;
+    }
+
+    public function getNodeId()
+    {
+        return $this->nodeId;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
     public function setToken($token) : void
     {
         $this->token = $token;
@@ -62,23 +83,60 @@ class EventMessage
         $this->body = $body;
     }
 
+    public function setServiceId($serviceId) : void
+    {
+        $this->serviceId = $serviceId;
+    }
+
+    public function setNodeId($nodeId) : void
+    {
+        $this->nodeId = $nodeId;
+    }
+
+    public function setCreatedAt($createdAt) : void
+    {
+        $this->createdAt = $createdAt;
+    }
+
     public function get()
     {
         return json_encode([
-            'version' => self::VERSION,
-            'token'   => $this->token,
-            'status'  => $this->status,
-            'message' => $this->message,
-            'body'    => $this->body
+            'version'   => self::VERSION,
+            'token'     => $this->getToken(),
+            'status'    => $this->getStatus(),
+            'message'   => $this->getMessage(),
+            'body'      => $this->getBody(),
+            'serviceId' => $this->getServiceId(),
+            'nodeId'    => $this->getNodeId(),
+            'createdAt' => $this->getCreatedAt()
         ]);
     }
 
-    public function set($eventMessage) : void
+    public function set($eventMessage) : bool
     {
         $em = json_decode($eventMessage);
-        $this->setToken($em['token']);
-        $this->setStatus($em['status']);
-        $this->setMessage($em['message']);
-        $this->setBody($em['body']);
+        if ($this->checkVersion($em['version'])) {
+            foreach ($em as $prop => $value) {
+                if (isset($this->$prop)) {
+                    $method = 'set' . ucfirst($prop);
+                    $this->$method($value);
+                }
+            }
+            return true;
+        }
+
+        return false;
+        //$this->setToken($em['token']);
+        //$this->setStatus($em['status']);
+        //$this->setMessage($em['message']);
+        //$this->setBody($em['body']);
+    }
+
+    private function checkVersion($version) : bool
+    {
+        if ($version === self::VERSION) {
+            return true;
+        }
+        return false;
     }
 }
